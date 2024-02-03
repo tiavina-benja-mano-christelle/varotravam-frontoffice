1bda1b29ebd1396020f9dfea136dad6bb7980c35
Man0212 <106870328+Man0212@users.noreply.github.com>
2024-02-03 14:16:09 +0300

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import '../assets/css/header.css';
+import '../assets/js/header.js';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png'
+
+function Header() {
+  return (
+    <nav class="navbar">
+        <div class="cont">
+            <div class="navbar-header">
+                <a href="#">
+                <img src={logo} style={{width:"120px"}}/>
+                </a>
+            </div>
+
+            <div class="navbar-menu" id="open-navbar1">
+            <ul class="navbar-nav">
+                <li><Link to={'/Profil'}>Profil</Link></li>
+                <li><Link to={'/Message'}>Message</Link></li>
+                <li><Link to={'/'}>Annonces</Link></li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
